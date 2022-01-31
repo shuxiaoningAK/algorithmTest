c11aee1a9691f66747538d74111c5934853bba7b
shu xiaoning <shuxiaoningAk@163.com>
2022-01-31 20:01:37 +0800

    add luogu

--- Luogu/P1035.cpp
+++ Luogu/P1035.cpp
@@ -8,5 +8,14 @@ int main() {
     int k;
     scanf("%d", &k);
 
-    
+    double sum = 0;
+    int i = 1;
+    int count = 0;
+    while (sum < (double)k) {
+        count ++;
+        sum += 1 / (double)i;
+        i++;
+    }
+
+    printf("%d", count);
 }
